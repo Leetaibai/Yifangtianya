e73f454f3e7bc389ccffc02c7edee0a3ba1396aa
Leetaibai <269867071@qq.com>
2016-09-22 17:05:16 +0800

    Add study-demo

---
+++ study-demo/music_player/js/list.js
@@ -0,0 +1,82 @@
+var playList = [
+		{
+			'artist' : '陈绮贞',
+			'musicName' : '旅行的意义',
+			'musicAlbum' : '<旅行的意义> 2004',
+            'musicURL' : 'http://m5.file.xiami.com/158/7158/351600/1769173994_825946_l.mp3?auth_key=6c2b7b2fb898379ae7ffc86661986a77-1474599600-0-null',
+            'avatarURL' : 'avatar/cqz.jpg',
+			'musicTime' : '244'
+		},
+		{
+			'artist' : '陈绮贞',
+			'musicName' : '太多',
+			'musicAlbum' : '<华丽的冒险> 2005',
+			'musicURL' : 'http://m5.file.xiami.com/414/1414/536664/1771303528_3687510_l.mp3?auth_key=4db89209ef44cb302b60d2bc6cea1109-1474599600-0-null',
+			'avatarURL' : 'avatar/cqz.jpg',
+			'musicTime' : '320'
+		},
+		{
+			'artist' : '陈奕迅',
+            'musicName' : '陪你度过漫长岁月',
+            'musicAlbum' : '<陪你度过漫长岁月> 2015',
+			'musicURL' : 'http://m5.file.xiami.com/135/135/2100216776/1774917404_58964790_l.mp3?auth_key=a88da2f0217f59e6dbe184734329107e-1474599600-0-null',
+			'avatarURL' : 'avatar/cyx.jpg',
+			'musicTime' : '242'
+		},
+		{
+			'artist' : '陈奕迅',
+			'musicName' : '好久不见',
+			'musicAlbum' : '<认了吧> 2007',
+            'musicURL' : 'http://m5.file.xiami.com/135/135/167589/2067242_2104493_l.mp3?auth_key=fd5d9d9c1b08e839ef56abe284e8a916-1474599600-0-null',
+            'avatarURL' : 'avatar/cyx.jpg',
+			'musicTime' : '252'
+		},
+		{
+			'artist' : '梁静茹',
+			'musicName' : '可惜不是你',
+			'musicAlbum' : '<现在开始我爱你> 2011',
+            'musicURL' : 'http://m5.file.xiami.com/836/1836/10063/124099_31583_l.mp3?auth_key=15b0f6ac7e9b55ea411bea5185282d1d-1474599600-0-null',
+            'avatarURL' : 'avatar/ljr.jpg',
+			'musicTime' : '285'
+		},
+		{
+			'artist' : '梁静茹',
+			'musicName' : '小手拉大手',
+			'musicAlbum' : '<听风的歌> 2010',
+            'musicURL' : 'http://m5.file.xiami.com/836/1836/10061/380246_18781_l.mp3?auth_key=7155deb6dfe73c62f6a8b404384061fc-1474599600-0-null',
+            'avatarURL' : 'avatar/ljr.jpg',
+			'musicTime' : '246'
+		},
+		{
+			'artist' : '王力宏',
+			'musicName' : '你不知道的事',
+			'musicAlbum' : '<火力全开> 2011',
+            'musicURL' : 'http://m5.file.xiami.com/169/7169/2100357847/1776208314_60358745_l.mp3?auth_key=0dfb4077304cc49bcce66674e94b0f4d-1474599600-0-null',
+            'avatarURL' : 'avatar/wlh.jpg',
+			'musicTime' : '313'
+		},
+		{
+			'artist' : '王力宏',
+			'musicName' : '需要人陪',
+			'musicAlbum' : '<十八般武艺> 2010',
+            'musicURL' : 'http://m5.file.xiami.com/906/906/2100359172/1776216472_60389105_l.mp3?auth_key=e27b38e9e97398f46eaae3e0aa0b161c-1474599600-0-null',
+            'avatarURL' : 'avatar/wlh.jpg',
+			'musicTime' : '271'
+		},
+		{
+			'artist' : '张惠妹',
+			'musicName' : '记得',
+			'musicAlbum' : '<真实> 2001',
+            'musicURL' : 'http://m5.file.xiami.com/197/1964894197/2100267048/1775549482_59621466_l.mp3?auth_key=45fad89f067209cc2de7f503c5528447-1474599600-0-null',
+            'avatarURL' : 'avatar/zhm.jpg',
+			'musicTime' : '322'
+		},
+		{
+			'artist' : '张惠妹',
+			'musicName' : '我最亲爱的',
+			'musicAlbum' : '<你在看我吗> 2011',
+            'musicURL' : 'http://m5.file.xiami.com/169/7169/462319/1770448368_2653490_l.mp3?auth_key=1098ea5a8113674f5674d7b877596e8d-1474599600-0-null',
+            'avatarURL' : 'avatar/zhm.jpg',
+			'musicTime' : '286'
+		}
+	];
